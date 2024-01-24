b7397c3c0d1c9f203c6c4a85d57c2fa7db346b06
Skyler-Haab1 . <laskyler412@gmail.com>
2024-01-24 18:13:31 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -17,7 +17,7 @@ function runProgram(){
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('eventType', handleKeyDown(event) {
-    console.log();
+    console.log("You pressed ",event.key);
   }                           // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
